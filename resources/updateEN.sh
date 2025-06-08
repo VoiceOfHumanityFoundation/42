6933c12b63d62b53ed7bda87c80a708190a094b6
KennyAwesome <you@example.com>
2025-06-08 10:58:49 +0200

    Updated Edition

--- resources/updateEN.sh
+++ resources/updateEN.sh
@@ -2,6 +2,6 @@ rm english.txt
 bash composeBook.sh > english.txt
 #pandoc en.txt -o ./book/42_en_latest.pdf
 pandoc -V geometry:margin=1in -o ./book/42_en_latest.pdf english.txt
-#git add ./book/42_en_latest.pdf
-#git commit -m "Updated edition"
-#git push
+git add ./book/42_en_latest.pdf
+git commit -m "Updated edition"
+git push
