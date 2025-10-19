0c03bca4c248dca3d2ad8dd3256563415f832038
KennyAwesome <you@example.com>
2025-10-19 16:01:55 +0200

    update

--- resources/main.py
+++ resources/main.py
@@ -22,13 +22,13 @@ import json
 from fastapi.responses import JSONResponse
 from fastapi.encoders import jsonable_encoder
 
-promptpath = "./draft/prompt.txt"
+promptpath = "../draft/prompt.txt"
 
 model_kwargs = {'trust_remote_code': True, 'device': 'cuda:0'}
 #model_kwargs = {'trust_remote_code': True, 'device': 'cpu'}
 embedder = HuggingFaceEmbeddings(model_name="nomic-ai/nomic-embed-text-v2-moe", model_kwargs=model_kwargs)
 print("Loading document...")
-loader = PyPDFLoader("./resources/book/42_en_latest.pdf")
+loader = PyPDFLoader("./book/42_en_latest.pdf")
 docs = loader.load()
 print("Loading model ok")
 # Split into chunks
