0512fba12976dfd9e5efe157c06d1d370c967fc7
KennyAwesome <you@example.com>
2025-11-22 16:28:21 +0100

    update

--- resources/updateEN.sh
+++ resources/updateEN.sh
@@ -1,5 +1,5 @@
 rm english.txt
-bash composeBook.sh > english.txt
+bash composeBook.sh | sed 's/\// \/ /g'  > english.txt
 #pandoc en.txt -o ./book/42_en_latest.pdf
 pandoc -V geometry:margin=1in -o ./book/42_en_latest.pdf english.txt
 git add ./book/42_en_latest.pdf
