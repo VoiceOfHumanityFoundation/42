eb7bb060c2934a1a2e35e132a8cff064be9b8c37
KennyAwesome <you@example.com>
2025-11-29 17:50:38 +0100

    update

--- resources/updateEN_A5.sh
+++ resources/updateEN_A5.sh
@@ -1,5 +1,5 @@
 rm english.txt
-bash composeBook.sh | sed 's/\// \/ /g'  > english.txt #need to break links for the margin
+bash composeBook.sh > english.txt #need to break links for the margin, probably need to make a different title.md
 #pandoc en.txt -o ./book/42_en_latest.pdf
 #pandoc -V geometry:margin=1in -o ./book/42_en_latest.pdf english.txt
 pandoc -V papersize:a5  -V geometry:margin=1in -o ./book/42_en_latest_A5.pdf english.txt
