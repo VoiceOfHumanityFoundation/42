e9c66297244b88f82089e38bc971324196e658cc
KennyAwesome <you@example.com>
2025-11-30 17:22:33 +0100

    added english A5 version

--- resources/updateEN_A5.sh
+++ resources/updateEN_A5.sh
@@ -1,8 +1,8 @@
-rm english.txt
-bash composeBook.sh > english.txt #need to break links for the margin, probably need to make a different title.md
+rm english_A5.txt
+bash composeBook.sh > english_A5.txt #need to break links for the margin, probably need to make a different title.md
 #pandoc en.txt -o ./book/42_en_latest.pdf
 #pandoc -V geometry:margin=1in -o ./book/42_en_latest.pdf english.txt
-pandoc -V papersize:a5  -V geometry:margin=1in -o ./book/42_en_latest_A5.pdf english.txt
+#pandoc -V papersize:a5  -V geometry:margin=1in -o ./book/42_en_latest_A5.pdf english_A5.txt
 #git add ./book/42_en_latest.pdf
 #git commit -m "Updated edition"
 #git push
